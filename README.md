e7852b83a83c4ab74fae1aed8bd53815428a5338
Alexandre Menard <alexandre.menard1@universite-paris-saclay.fr>
2025-06-11 10:45:07 +0200

    Push last 2 weeks of work

---
+++ README.md
@@ -0,0 +1,37 @@
+<br />
+<div align="center">
+    <a href="https://github.com/alexandremnd/CollectiveScattering">
+        <img src="img/illustration.png" alt="Logo" width="180" height="180">
+    </a>
+    <h3 align="center">Collective Scattering</h3>
+    <p align="center">
+        A semi-classical approach to compute scattered field in cold atom gases organized in periodic medium.
+    </p>
+</div>
+
+# Getting started
+## Cloning and building the project
+
+1. Clone the repository in the directory of your choice:
+```bash
+    git clone https://github.com/alexandremnd/CollectiveScattering.git
+    cd CollectiveScattering
+```
+
+2. Set parameters in main.jl for desired periodic medium properties (density/radius/atoms count/...) and use mean function:
+```bash
+    julia main.jl
+```
+
+
+# Authors
+- [Alexandre Menard](https://github.com/alexandremnd/)
+
+# Contributing
+Contributions are currently not expected as this is an internship project.
+
+# References
+
+
+# License
+This project is licensed under the GNU GPLv3 - see [LICENCE](LICENSE) file for details
--- main.jl
+++ main.jl
@@ -1,11 +1,95 @@
-using BenchmarkTools
-using Plots
-using CUDA
-using Distances
-using LinearAlgebra
+using CUDA, GPUArrays
+using DifferentialEquations
 using ProgressBars
+using LinearAlgebra
+using BenchmarkTools
 
-struct GaussianBeam{T<:Real}
+
+# =============== General utility functions ===============
+struct SimulationParameters{T <: Real, N <: Integer}
+    Na::N
+    Nd::N
+    Rd::T
+    a::T
+    d::T
+    Δ0::T
+end
+
+function bragg_periodicity(θ)
+    return convert(eltype(θ), 0.5) /  cos(θ)
+end
+
+function build_xOz_plane(Np, size)
+    X = range(-size/2, size/2, length=Np)' .* ones(Np)
+    Z = ones(Np)' .* range(-size/2, size/2, length=Np)
+
+    return X, Z
+end
+
+function build_sphere_region(R, theta, phi)
+    X = R .* sin.(theta) .* cos.(phi)'
+    Y = R .* sin.(theta) .* sin.(phi)'
+    Z = R .* cos.(theta) .* ones(length(phi))'
+
+    return X, Y, Z
+end
+
+function save_matrix_to_csv(matrix, filename)
+    open(filename, "w") do file
+        for row in eachrow(matrix)
+            println(file, join(row, ","))
+        end
+    end
+end
+
+# =============== Optical lattice generation functions ===============
+function uniform_optical_lattice!(params::SimulationParameters{T, N}, scatterers, centered=true) where {T <: Real, N <: Integer}
+    @inbounds @fastmath for i in axes(scatterers, 1)
+        u = rand()
+        theta = 2π * rand()
+        disk_no = rand(0:(params.Nd -1))
+
+        scatterers[i, 1] = params.Rd * sqrt(u) * cos(theta)
+        scatterers[i, 2] = params.Rd * sqrt(u) * sin(theta)
+        scatterers[i, 3] = disk_no * params.d + params.a * 2.0 * (rand() - 0.5) - params.a / 2.0
+
+        if centered
+            scatterers[i, 3] -= (params.Nd - 1) * params.d / 2.0
+        end
+    end
+
+    return
+end
+
+
+function uniform_optical_lattice!(params::SimulationParameters{B, N}, scatterers::T, centered=true) where {B <: Real, N <: Integer, T <: AbstractGPUArray}
+    CUDA.@sync @cuda threads=1024 blocks=cld(Na, 1024) _uniform_optical_lattice!(params, scatterers, centered)
+end
+
+function _uniform_optical_lattice!(params::SimulationParameters{B, N}, scatterers, centered=true) where {B <: Real, N <: Integer}
+    i = (blockIdx().x - Int32(1)) * blockDim().x + threadIdx().x
+
+    if i > size(scatterers, 1)
+        return nothing
+    else
+        u = rand(Float32)
+        theta = 2.0f0π * rand(Float32)
+        disk_no = rand(0:(params.Nd - 1))
+
+        @inbounds @fastmath scatterers[i, Int32(1)] = params.Rd * sqrt(u) * cos(theta)
+        @inbounds @fastmath scatterers[i, Int32(2)] = params.Rd * sqrt(u) * sin(theta)
+        @inbounds scatterers[i, Int32(3)] = disk_no * params.d + params.a * 2.0f0 * (rand(Float32) - 0.5f0) - params.a / 2.0f0
+
+        if centered
+            @inbounds scatterers[i, Int32(3)] -= (params.Nd - one(params.Nd)) * params.d / 2.0f0
+        end
+    end
+
+    return nothing
+end
+
+# =============== Beam and field computation functions ===============
+struct GaussianBeam{T <: Real}
     E0::T
     w0::T
     θ::T
@@ -24,52 +108,7 @@ struct GaussianBeam{T<:Real}
 end
 GaussianBeam(E0, w0, θ) = GaussianBeam(E0, w0, θ, -sin(θ), zero(eltype(θ)), cos(θ), cos(θ), zero(eltype(θ)), sin(θ), zero(eltype(θ)), one(eltype(θ)), zero(eltype(θ)))
 
-
-function bragg_periodicity(θ)
-    return 1.0 / (2.0 * cos(θ))
-end
-
-function optical_lattice!(Nd, Rd, a, d, scatterers; centered=true)
-    @inbounds @fastmath @simd for i in axes(scatterers, 1)
-        u = rand()
-        theta = 2π * rand()
-        disk_no = rand(0:(Nd-1))
-
-        scatterers[i, 1] = Rd * sqrt(u) * cos(theta)
-        scatterers[i, 2] = Rd * sqrt(u) * sin(theta)
-        scatterers[i, 3] = disk_no * d + a * 2.0 * (rand() - 0.5) - a / 2.0
-
-        if centered
-            scatterers[i, 3] -= (Nd - 1) * d / 2.0
-        end
-    end
-
-    return
-end
-
-function optical_lattice!(Nd, Rd, a, d, scatterers::CuDeviceMatrix{T, 1}; centered=true) where T <:Real
-    i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-
-    if i > size(scatterers, 1)
-        return
-    end
-
-    u = rand(Float32)
-    theta = 2.0f0π * rand(Float32)
-    disk_no = rand(0:(Nd-1))
-
-    @inbounds scatterers[i, 1] = Rd * sqrt(u) * cos(theta)
-    @inbounds scatterers[i, 2] = Rd * sqrt(u) * sin(theta)
-    @inbounds scatterers[i, 3] = disk_no * d + a * 2.0f0 * (rand(Float32) - 0.5f0) - a / 2.0f0
-
-    if centered
-        @inbounds scatterers[i, 3] -= (Nd - 1) * d / 2.0f0
-    end
-
-    return nothing
-end
-
-function compute_field(x, y, z, field::GaussianBeam{T})::ComplexF32 where T<:Real
+function compute_field(x, y, z, field)
     proj_z = field.kx * x + field.ky * y + field.kz * z
 
     proj_r2 = (field.u2x * x + field.u2y * y + field.u2z * z)^2 +
@@ -89,111 +128,172 @@ function compute_field(x, y, z, field::GaussianBeam{T})::ComplexF32 where T<:Rea
 end
 
 function compute_scattered_field(x, y, z, scatterers, amplitudes)
-    res = zero(Complex{eltype(x)}) # Ensure the precison matches the input precision
+    res = zero(Complex{eltype(x)})
 
-    for i in axes(scatterers, 1)
-        @inbounds dist = (scatterers[i, 1] - x)^2 + (scatterers[i, 2] - y)^2 + (scatterers[i, 3] - z)^2
+    @fastmath @inbounds for i in axes(scatterers, 1)
+        dist = (scatterers[i, 1] - x)^2 + (scatterers[i, 2] - y)^2 + (scatterers[i, 3] - z)^2
         dist = sqrt(dist)
-        @inbounds res -= exp(2.0f0im * π * dist) / (2.0f0 * π * dist) * amplitudes[i]
+        res -= cispi(2.0 * dist) / (2.0 * π * dist) * amplitudes[i]
     end
 
     return res
 end
 
-function compute_system_matrix!(scatterers, M, Δ0)
-    pairwise!(Euclidean(), M, scatterers, dims=1)
+# =============== System matrix computation functions ===============
+function distance!(out, scatterers, factor=1.0)
+    @inbounds @fastmath for i in axes(out, 2)
+        xi = scatterers[i, 1]
+        yi = scatterers[i, 2]
+        zi = scatterers[i, 3]
 
-    @. M = cispi(2. * M) / (2.0π * 1.0im * M)
+        @simd for j in 1:i
+            diff = (xi - scatterers[j, 1])^2 +
+                    (yi - scatterers[j, 2])^2 +
+                    (zi - scatterers[j, 3])^2
 
-    for i in 1:Na
-        @inbounds M[i, i] = 1.0 - 2.0im * Δ0
+            out[j, i] = factor * sqrt(diff)
+        end
     end
-
-    M .*= -0.5
-    M = Symmetric(M)
-
-    return
 end
 
-function compute_system_matrix!(scatterers::CuDeviceMatrix{Float32, 1}, M::CuDeviceMatrix{ComplexF32, 1}, Δ0)
-    i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-    j = (blockIdx().y - 1) * blockDim().y + threadIdx().y
+function compute_system_matrix!(M, dist, Δ0)
+    @inbounds @fastmath for i in axes(M, 2)
+        for j in 1:(i-1)
+            M[j, i] = 0.5im * cis(dist[j, i]) / (dist[j, i])
+            M[i, j] = M[j, i]  # Ensure symmetry
+        end
+    end
+
+    @inbounds @fastmath @simd for i in axes(M, 1)
+        M[i, i] = -0.5 + 1.0im * params.Δ0
+    end
+end
+
+function compute_system_matrix!(scatterers::T, M, Δ0) where {T <: AbstractGPUArray}
+    m = size(M, 1)
+    @cuda threads=(32, 32) blocks=(cld(m, 32), cld(m, 32)) _compute_system_matrix!(scatterers, M, Δ0)
+end
+
+function _compute_system_matrix!(scatterers, M, Δ0)
+    i = (blockIdx().x - Int32(1)) * blockDim().x + threadIdx().x
+    j = (blockIdx().y - Int32(1)) * blockDim().y + threadIdx().y
 
     m, n = size(M)
     if i > m || j > n
         return nothing
-    end
-
-    if i == j
+    elseif i == j
         @inbounds M[i, j] = -0.5f0 + 1.0f0im * Δ0
-        return nothing
+    else
+        dist = zero(eltype(M))
+
+        k::Int32 = 1
+        while k <= 3
+            @inbounds diff = scatterers[i, k] - scatterers[j, k]
+            dist += diff * diff
+
+            k += one(k)
+        end
+
+        @fastmath dist = sqrt(dist)
+        @inbounds @fastmath M[i, j] = -0.5f0 * exp(2.0f0im * π * dist) / (2.0f0im * π * dist)
+
     end
-
-    dist = zero(eltype(M))
-    for k in 1:3
-        @inbounds diff = scatterers[i, k] - scatterers[j, k]
-        dist += diff * diff
-    end
-
-    @inbounds M[i, j] = sqrt(dist)
-    @inbounds M[i, j] = -0.5f0 * exp(2.0f0im * π * M[i, j]) / (2.0f0im * π * M[i, j])
-
-    return
+    return nothing
 end
 
+# =============== Averaging function ===============
+"""
+For a given simulation parameters, will average intensity of "iterations" configurations on CPU or GPU depending on memory location of X.
+"""
+function mean_intensity(params::SimulationParameters{P, N}, incident_field::GaussianBeam{P}, X, Y, Z; iterations=100) where {P <: Real, N <: Integer}
+    scatterers = similar(X, params.Na, 3)
+    M = similar(X, Complex{eltype(X)}, params.Na, params.Na)
+    E = similar(X, Complex{eltype(X)}, params.Na)
+    A = similar(X, Complex{eltype(X)}, params.Na)
+    intensity = zero(X)
 
-# ============ Main program ============
-# Atomic parameters
+    for i in ProgressBar(1:iterations)
+        uniform_optical_lattice!(params, scatterers)
+        compute_system_matrix!(scatterers, M, params.Δ0)
+
+        E .= convert(eltype(M), 0.5im) .* compute_field.(scatterers[:, 1], scatterers[:, 2], scatterers[:, 3], Ref(incident_field))
+
+        A .= M \ E
+
+        intensity .+= abs2.(compute_field.(X, Y, Z, Ref(incident_field)) .+ compute_scattered_field.(X, Y, Z, Ref(scatterers), Ref(A)))
+    end
+    intensity ./= iterations * incident_field.E0^2
+
+    return intensity
+end
+
+function f(du, u, p, t)
+    mul!(du, p, u)
+    return nothing
+end
+
+function dynamic_intensity(params::SimulationParameters{N, P}, incident_field::GaussianBeam{P}, t_span, X, Y, Z; iterations=100) where {P <: Real, N <: Integer}
+    scatterers = similar(X, params.Na, 3)
+    M = similar(X, Complex{eltype(X)}, params.Na, params.Na)
+    dist = similar(X, Float64, params.Na, params.Na)
+    E = similar(X, Complex{eltype(X)}, params.Na)
+    A = similar(X, Complex{eltype(X)}, params.Na)
+    field = zero(X)
+    intensity = zeros(size(X, 1), length(t_span))
+
+    for i in 1:iterations
+        uniform_optical_lattice!(params, scatterers)
+        distance!(dist, scatterers, 2.0π)
+        compute_system_matrix!(M, dist, params.Δ0)
+
+        E .= convert(eltype(M), 0.5im) .* compute_field.(view(scatterers, :, 1), view(scatterers, :, 2), view(scatterers, :, 3), Ref(incident_field))
+        A .= M \ E
+
+        problem = ODEProblem(f, A, (t_span[begin], t_span[end]), M)
+        sol = solve(problem, Tsit5())
+
+        k = 1
+        for t in t_span
+            A .= sol(t)
+            field .= abs2.(compute_field.(X, Y, Z, Ref(incident_field)) .+ compute_scattered_field.(X, Y, Z, Ref(scatterers), Ref(A)))
+            intensity[:, k] += sum(field, dims=2) / size(field, 2)
+            k += 1
+        end
+
+    end
+    intensity ./= intensity[:, 1]
+
+    return intensity
+end
+
+# ================== Main ==================
+# Simulation parameters
 Na  = 1000
-Nd  = 50
-Rd  = 9
-a   = 0.01
-Δ0  = 0f0
-
+Nd  = 20
+Rd  = 9.0
+a   = 0.07
+Δ0  = 0.0
 
 # Electric field parameters
 E0  = 1e-3
 w0  = 4.0
-θ   = deg2rad(15)
-d   = bragg_periodicity(deg2rad(15.0))
+θ   = deg2rad(90)
+d   = bragg_periodicity(deg2rad(30))
+
+# Others parameters
+nb_iterations = 1000
+
+# Computation
+
 incident_field = GaussianBeam(E0, w0, θ)
+params = SimulationParameters(Na, Nd, Rd, a, d, Δ0)
 
-# Final field parameters
-Np  = 1000
+if_d = GaussianBeam(Float32(E0), Float32(w0), Float32(θ))
+params_d = SimulationParameters{Float32, Int32}(Na, Nd, Float32(Rd), Float32(a), Float32(d), Float32(Δ0))
+X, Z = build_xOz_plane(250, 90)
 
-# ============ Stationnary state computation ============
-scatterers_d = CuMatrix{Float32}(undef, Na, 3)
-M_d = CuMatrix{ComplexF32}(undef, Na, Na)
-E_d = CuVector{ComplexF32}(undef, Na)
-A_d = CuVector{ComplexF32}(undef, Na)
-field_d = CUDA.zeros(Float32, Np, Np)# CuMatrix{Float32}(undef, Np, Np)
-
-Z = range(-60f0, 60f0, length=Np)' .* ones(Np)
-X = ones(Np)' .* range(-60f0, 60f0, length=Np)
 X_d = CuMatrix{Float32}(X)
 Z_d = CuMatrix{Float32}(Z)
 
-for i in ProgressBar(1:1000)
-    CUDA.@sync @cuda threads=1024 blocks=cld(Na, 1024) optical_lattice!(Nd, Rd, a, d, scatterers_d)
-    @cuda threads=(32, 32) blocks=(cld(Na, 32), cld(Na, 32)) compute_system_matrix!(scatterers_d, M_d, Δ0)
-    E_d .= 0.5f0im .* compute_field.(scatterers_d[:, 1], scatterers_d[:, 2], scatterers_d[:, 3], Ref(incident_field))
-    CUDA.synchronize()
-
-    A_d .= M_d \ E_d
-    field_d .+= abs2.(compute_field.(X_d, 0f0, Z_d, Ref(incident_field)) .+ compute_scattered_field.(X_d, 0f0, Z_d, Ref(scatterers_d), Ref(A_d)))
-end
-# ============ Final field computation ============
-field_d ./= 1000.0f0
-
-
-
-
-
-# Create heatmap
-heatmap(X[:,1], Z[1,:], Array(field_d),
-    xlabel="Z", ylabel="X",
-    title="Gaussian Beam Intensity",
-    colorbar_scale=:log10,
-    aspect_ratio=:equal,
-    color=:thermal)
+intensity = mean_intensity(params, incident_field, X_d, 0f0, Z_d; iterations=nb_iterations)
 
